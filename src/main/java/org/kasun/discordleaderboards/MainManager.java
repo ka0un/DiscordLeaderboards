039b44e28b780dcc1c4b2e7972e6bd7ca7f26619
BuildTools <unconfigured@null.spigotmc.org>
2023-05-27 02:24:38 +0500

    Created a single command Added the reload system and the reload command

--- src/main/java/org/kasun/discordleaderboards/MainManager.java
+++ src/main/java/org/kasun/discordleaderboards/MainManager.java
@@ -1,6 +1,7 @@
 package org.kasun.discordleaderboards;
 
 import org.bukkit.Bukkit;
+import org.bukkit.event.HandlerList;
 import org.kasun.discordleaderboards.Commands.CommandsManager;
 import org.kasun.discordleaderboards.Configs.ConfigManager;
 import org.kasun.discordleaderboards.Database.DatabaseManager;
@@ -9,6 +10,8 @@ import org.kasun.discordleaderboards.Listeners.ListenerManager;
 import org.kasun.discordleaderboards.Schedules.ScheduleManager;
 
 public class MainManager {
+
+    private final DiscordLeaderboards plugin = DiscordLeaderboards.getInstance();
     private ConfigManager configManager;
     private DatabaseManager databaseManager;
     private CommandsManager commandsManager;
@@ -20,10 +23,71 @@ public class MainManager {
         scheduleManager = new ScheduleManager();
         configManager = new ConfigManager();
         databaseManager = new DatabaseManager();
-        commandsManager = new CommandsManager();
+        commandsManager = new CommandsManager(this);
         listenerManager = new ListenerManager();
         if (Bukkit.getPluginManager().getPlugin("DiscordSRV") != null){
             discordSrvManager = new DiscordSrvManager();
         }
+    }
+
+    public void reload() {
+        HandlerList.unregisterAll(plugin);
+        Bukkit.getScheduler().cancelTasks(plugin);
+        setScheduleManager(new ScheduleManager());
+        setConfigManager(new ConfigManager());
+        setDatabaseManager(new DatabaseManager());
+        setCommandsManager(new CommandsManager(this));
+        setListenerManager(new ListenerManager());
+        if (Bukkit.getPluginManager().getPlugin("DiscordSRV") != null){
+            setDiscordSrvManager(new DiscordSrvManager());
+        }
+    }
+
+    public ConfigManager getConfigManager() {
+        return configManager;
+    }
+
+    public void setConfigManager(ConfigManager configManager) {
+        this.configManager = configManager;
+    }
+
+    public DatabaseManager getDatabaseManager() {
+        return databaseManager;
+    }
+
+    public void setDatabaseManager(DatabaseManager databaseManager) {
+        this.databaseManager = databaseManager;
+    }
+
+    public CommandsManager getCommandsManager() {
+        return commandsManager;
+    }
+
+    public void setCommandsManager(CommandsManager commandsManager) {
+        this.commandsManager = commandsManager;
+    }
+
+    public ListenerManager getListenerManager() {
+        return listenerManager;
+    }
+
+    public void setListenerManager(ListenerManager listenerManager) {
+        this.listenerManager = listenerManager;
+    }
+
+    public DiscordSrvManager getDiscordSrvManager() {
+        return discordSrvManager;
+    }
+
+    public void setDiscordSrvManager(DiscordSrvManager discordSrvManager) {
+        this.discordSrvManager = discordSrvManager;
+    }
+
+    public ScheduleManager getScheduleManager() {
+        return scheduleManager;
+    }
+
+    public void setScheduleManager(ScheduleManager scheduleManager) {
+        this.scheduleManager = scheduleManager;
     }
 }
