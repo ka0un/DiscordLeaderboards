112e5daba6ddaca88bd0b51f73674665f3b3f2db
KASUNHapangama <88395585+KASUNHapangama@users.noreply.github.com>
2023-05-27 21:20:50 +0530

    - Added HigherIsBetter

--- src/main/java/org/kasun/discordleaderboards/Leaderboard/TopList.java
+++ src/main/java/org/kasun/discordleaderboards/Leaderboard/TopList.java
@@ -24,19 +24,13 @@ public class TopList {
 
     public Map<String, Integer> getTopListAsMap() {
         int top = leaderboardConfig.getTop();
-        return SqlUtils.getTopPlayerMap(placeholderColumnName, top);
+        return SqlUtils.getTopPlayerMap(placeholderColumnName, top, leaderboardConfig.isHigherisbetter());
 
     }
 
     public String getTopListAsStringForWebhook() {
         Map<String, Integer> toplistmap = getTopListAsMap();
-        // find the maximum length of the names
-        int maxNameLength = 0;
-        for (String name : toplistmap.keySet()) {
-            if (name.length() > maxNameLength) {
-                maxNameLength = name.length();
-            }
-        }
+        int maxNameLength = getmaxnamelenght(toplistmap);
         StringBuilder sb = new StringBuilder();
         int i = 1;
         for (Map.Entry<String, Integer> entry : toplistmap.entrySet()) {
@@ -51,16 +45,28 @@ public class TopList {
 
     public String getTopListAsString() {
         Map<String, Integer> toplistmap = getTopListAsMap();
+        int maxNameLength = getmaxnamelenght(toplistmap);
         StringBuilder sb = new StringBuilder();
         int i = 1;
         for (Map.Entry<String, Integer> entry : toplistmap.entrySet()) {
             String name = entry.getKey();
             int score = entry.getValue();
-            String formattedEntry = String.format("%d. %-20s %d%s\n", i++, name, score, leaderboardConfig.getMetric());
+            String formattedEntry = String.format("%d. %-" + (maxNameLength + 3) + "s %d%s\n", i++, name, score, leaderboardConfig.getMetric());
             sb.append(formattedEntry);
         }
         String leaderboardString = sb.toString();
         return leaderboardString;
+    }
+
+    private int getmaxnamelenght(Map<String, Integer> toplistmap){
+        // find the maximum length of the names
+        int maxNameLength = 0;
+        for (String name : toplistmap.keySet()) {
+            if (name.length() > maxNameLength) {
+                maxNameLength = name.length();
+            }
+        }
+        return maxNameLength;
     }
 
 
